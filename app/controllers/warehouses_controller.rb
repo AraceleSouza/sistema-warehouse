5e705a06c061351641debc6b1c6ac5df34d17416
Aracele Souza <aracele-souza@hotmail.com>
2022-05-05 15:17:23 -0300

    Usuário cadastra um galpão

--- app/controllers/warehouses_controller.rb
+++ app/controllers/warehouses_controller.rb
@@ -3,4 +3,18 @@ class WarehousesController < ApplicationController
         id = params[:id]
         @warehouse = Warehouse.find(id)
     end
+    
+    def new
+    end
+
+    def create
+      # Aqui dentro que nós vamos:
+      # 1- Receber os dados enviados
+      # 2- Criar um novo galpão no banco de dados
+      warehouse_params = params.require(:warehouse).permit(:name, :code, :city, :description, :address, :cep, :area)
+      warehouse =  Warehouse.new(warehouse_params)
+      warehouse.save()
+      # 3 - Redirecionar para tela inicial 
+      redirect_to root_path
+    end
 end
