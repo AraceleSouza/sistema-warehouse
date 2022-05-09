c452234a5bf1b800c4964e32a9c16bc7bae1e84d
Aracele Souza <aracele-souza@hotmail.com>
2022-05-09 11:41:55 -0300

    Usuário edita um galpão e mantém os campos obrigatórios

--- app/controllers/warehouses_controller.rb
+++ app/controllers/warehouses_controller.rb
@@ -23,6 +23,23 @@ class WarehousesController < ApplicationController
       end
       # 3 - Redirecionar para tela inicial 
       #flash[:notice] = "Galpão cadastrado com sucesso."
-     
+    end
+    
+    def edit 
+      id = params[:id]
+      @warehouse = Warehouse.find(id)
+    end
+
+    def update 
+      id = params[:id]
+      @warehouse = Warehouse.find(id)
+      warehouse_params = params.require(:warehouse).permit(:name, :code, :city, :description, :address, 
+                                                           :cep, :area)
+      if @warehouse.update(warehouse_params)
+      redirect_to warehouse_path(@warehouse.id), notice: 'Galpão atualizado com sucesso'
+      else
+        flash.now[:notice] = 'Não foi possível atualizar o galpão'
+        render 'edit'
+      end
     end
 end
