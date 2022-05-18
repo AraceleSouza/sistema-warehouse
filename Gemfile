a5b7294e8c48ac2951e0d3f4e0a64cf788a22b76
Aracele Souza <aracele-souza@hotmail.com>
2022-05-18 14:02:21 -0300

    Usuário cadastra um modelo de produto

--- Gemfile
+++ Gemfile
@@ -24,6 +24,7 @@ group :development, :test do
   gem "rspec-rails"
   gem "capybara"
 
+
 end
 
 group :development do
---
+++ spec/system/warehouses/user_register_product_model_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+describe 'User registers a product model' do 
+  it 'successfully' do
+    # Arrange
+    supplier = Supplier.create!(brand_name: 'LG', corporate_name: 'LG Eletronicos LTDA', 
+                                registration_number: '43447215000102', full_address: 'Av Doutor Arnaldo, 125',
+                                city: 'São Paulo', state: 'SP', email: 'sac@lgeletronicos.com')
+    # Act
+    visit root_path
+    click_on 'Modelos de Produtos'
+    click_on 'Cadastrar Novo'
+    fill_in 'Nome', with: 'TV 32 polegadas'
+    fill_in 'Altura', with: '45'
+    fill_in 'Largura', with: '70'
+    fill_in 'Peso', with: '8_000'
+    fill_in 'Profundidade', with: '10'
+    fill_in 'SKU', with: 'TV40-LG-XPT090569743'
+    select 'LG', from: 'Fornecedor'
+    click_on 'Enviar'
+
+  
+    # Assert
+    expect(page).to have_content 'Modelo de produto cadastrado com sucesso.'
+    expect(page).to have_content 'TV 32 polegadas'
+    expect(page).to have_content 'Fornecedor: LG'
+    expect(page).to have_content 'SKU: TV40-LG-XPT090569743'
+    expect(page).to have_content 'Dimensão: 45cm x 70cm x 10cm'
+    expect(page).to have_content 'Peso: 8000g'
+  end
+
+ 
+end
