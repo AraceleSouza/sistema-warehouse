2804a1c755a44d878e8ccf99340f35b37826b445
Aracele Souza <aracele-souza@hotmail.com>
2022-05-19 19:28:54 -0300

    Usuário se autentica com sucesso

--- spec/system/warehouses/user_sign_in_spec.rb
+++ spec/system/warehouses/user_sign_in_spec.rb
@@ -12,14 +12,31 @@ describe 'User authenticates' do
       fill_in 'Senha', with: 'password'
       click_on 'Entrar'
     end
-
     # Assert
     expect(page).to have_content 'Login efetuado com sucesso.'
     within('nav') do
       expect(page).not_to have_link 'Entrar'
-      expect(page).to have_link 'Sair'
+      expect(page).to have_button 'Sair'
       expect(page).to have_content 'aracele@email.com'
     end
-    
+  end  
+
+  it 'and logout' do
+    # Arrange
+    User.create!(email: 'aracele@email.com', password: 'password')
+    # Act
+    visit root_path
+    click_on 'Entrar'
+    within('form') do
+      fill_in 'E-mail', with: 'aracele@email.com'
+      fill_in 'Senha', with: 'password'
+      click_on 'Entrar'
+    end
+    click_on 'Sair'
+    # Assert
+    expect(page).to have_content 'Logout efetuado com sucesso.'
+    expect(page).to have_link 'Entrar'
+    expect(page).not_to have_button 'Sair'
+    expect(page).not_to have_content 'aracele@email.com'
   end
 end
