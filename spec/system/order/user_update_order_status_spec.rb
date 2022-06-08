c1a9369c1b1b750217ad776e6810c2cf60df640f
Aracele Souza <aracele-souza@hotmail.com>
2022-06-07 22:46:39 -0300

    Pedido com status Cancelado

--- spec/system/order/user_update_order_status_spec.rb
+++ spec/system/order/user_update_order_status_spec.rb
@@ -26,5 +26,25 @@ describe 'User informs new order status' do
   end
 
   it 'and order was canceled' do
+    # Arrange
+    carla = User.create!(name: 'Carla', email: 'carla@email.com', password:'123654')    
+    warehouse = Warehouse.create!(name: 'Aeroporto SP', code: 'GRU', city: 'Guarulhos', area: 100_000,
+                                  address: 'Avenida do Aeroporto, 1000', cep: '15000-000', 
+                                  description: 'Galpão destinado para cargas internacionais') 
+    supplier = Supplier.create!(corporate_name: 'World Technology Vision LTDA', brand_name: 'TECH VISION', 
+                                registration_number: '43447216000102', full_address: 'Av das Flores, 500', 
+                                city: 'Cajamar', state:'SP', email: 'tech_vision@gmail.com')
+    order = Order.create!(user: carla , warehouse: warehouse, supplier:supplier, 
+                                estimated_delivery_date: 1.day.from_now, status: :pending)
+    # Act
+    login_as(carla)
+    visit root_path
+    click_on 'Meus Pedidos'
+    click_on order.code
+    click_on 'Marcar como CANCELADO'
+
+    # Assert
+    expect(current_path).to eq order_path(order.id)
+    expect(page).to have_content('Situação do Pedido: Cancelado')
   end
 end
