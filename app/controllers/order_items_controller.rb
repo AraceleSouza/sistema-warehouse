c8afc6307476787c21bd9e6ffac4cc72310727f8
Aracele Souza <aracele-souza@hotmail.com>
2022-06-09 11:59:56 -0300

    Testes para validação de quantidade do item obrigatória

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -8,7 +8,14 @@ class OrderItemsController < ApplicationController
   def create
     @order = Order.find(params[:order_id])
     order_item_params = params.require(:order_item).permit(:product_model_id, :quantity)
-    @order.order_items.create(order_item_params)
-    redirect_to @order, notice: 'Item adicionado com sucesso'
+    @order_item = OrderItem.new(order_item_params)
+    @order_item.order = @order
+    if @order_item.save
+      redirect_to @order, notice: 'Item adicionado com sucesso'
+    else
+      @products = @order.supplier.product_models
+      flash.now[:notice] = 'Não foi possível registrar o pedido.'
+      render 'new'
+    end
   end
 end
--- spec/models/order_item_spec.rb
+++ spec/models/order_item_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe OrderItem, type: :model do
-  
+  describe '#valid?' do
+    it 'quantity must be mandatory' do
+      # Arrange
+      order_item = OrderItem.new(quantity: ' ')
+      # Act
+      order_item.valid?
+      result = order_item.errors.include? (:quantity)
+      # Assert
+      expect(result).to be true
+    end
+  end    
 end
