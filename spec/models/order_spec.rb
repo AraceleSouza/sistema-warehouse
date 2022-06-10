3b56252161495869ce3219a7bb57a04c67cff42b
Aracele Souza <aracele-souza@hotmail.com>
2022-06-09 22:28:26 -0300

    Código aleatório não deve ser modificado

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -102,5 +102,23 @@ RSpec.describe Order, type: :model do
       # Assert
       expect(second_order.code).not_to eq first_order.code     
     end
+
+    it 'and must not be modified' do
+      # Arrange
+      user = User.create!(name: 'Sergio', email: 'sergio@email.com', password:'123456')
+      warehouse = Warehouse.create!(name:'Rio', code: 'RIO', address: 'Endereço',
+                                  cep: '25000-000', city: 'Rio', area: 1000, 
+                                  description: 'Alguma descrição')
+      supplier = Supplier.create!(corporate_name: 'World Technology Vision LTDA', brand_name: 'TECH VISION', 
+                                registration_number: '43447216000102', full_address: 'Av das Flores, 500', 
+                                city: 'Cajamar', state:'SP', email: 'tech_vision@gmail.com')
+      order = Order.create!(user: user, warehouse: warehouse, supplier: supplier, 
+                        estimated_delivery_date: 1.week.from_now)
+      original_code = order.code
+      # Act
+      order.update!(estimated_delivery_date: 1.month.from_now)
+      # Assert
+      expect(order.code).to eq(original_code)
+    end
   end
 end
