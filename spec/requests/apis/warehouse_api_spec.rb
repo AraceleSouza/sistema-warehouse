04c911ac8027b71b8a3353d486c0cf44cb58129e
Aracele Souza <aracele-souza@hotmail.com>
2022-06-19 16:24:08 -0300

    Melhoria no teste para visualização de informações da API

--- spec/requests/apis/warehouse_api_spec.rb
+++ spec/requests/apis/warehouse_api_spec.rb
@@ -3,18 +3,29 @@ require 'rails_helper'
 describe 'Warehouse API' do
   context 'GET /api/v1/warehouse/1' do
     it 'success' do
-    # Arrange
-    warehouse = Warehouse.create!(name: 'Aeroporto SP', code: 'GRU', city: 'Guarulhos', area: 100_000,
-                                address: 'Avenida do Aeroporto, 1000', cep: '15000-000', 
-                                description: 'Galpão destinado para cargas internacionais')
-    # Act
-    get "/api/v1/warehouses/#{warehouse.id}"
-    # Assert
-    expect(response.status).to eq 200
-    expect(response.content_type).to include 'application/json'
-    json_response = JSON.parse(response.body)
-    expect(json_response["name"]).to eq('Aeroporto SP')
-    expect(json_response["code"]).to eq('GRU')
+      # Arrange
+      warehouse = Warehouse.create!(name: 'Aeroporto SP', code: 'GRU', city: 'Guarulhos', area: 100_000,
+                                  address: 'Avenida do Aeroporto, 1000', cep: '15000-000', 
+                                  description: 'Galpão destinado para cargas internacionais')
+      # Act
+      get "/api/v1/warehouses/#{warehouse.id}"
+      # Assert
+      expect(response.status).to eq 200
+      expect(response.content_type).to include 'application/json'
+      json_response = JSON.parse(response.body)
+      expect(json_response["name"]).to eq('Aeroporto SP')
+      expect(json_response["code"]).to eq('GRU')
+      expect(json_response.keys).not_to include("created_at")
+      expect(json_response.keys).not_to include("updated_at")
+    end
+
+    it 'fail' do
+      # Arrange
+  
+      # Act
+  
+      # Assert
+  
     end
   end
 end
